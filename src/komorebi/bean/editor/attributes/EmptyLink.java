ce7ef55192ffed6f88597eb543482fa78ea27c28
fberry26 <afberry526@gmail.com>
2017-07-21 16:10:12 -0400

    Button linking and platforms

--- src/komorebi/bean/editor/attributes/EmptyLink.java
+++
@@ -1,9 +0,0 @@
-package komorebi.bean.editor.attributes;
-
-public class EmptyLink extends Link {
-  
-  public EmptyLink(int x, int y, ClickButton button) {
-    super(null, "Nothing", x, y, button);
-  }
- 
-}
